b8ab2d719c7e6b0517ac3ff6c111dd38b2d234eb
mankind4dev <ogunmakin120@gmail.com>
2024-01-09 14:13:33 +0100

    connect my mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from"dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB perfectly");
+}).catch((error) => {
+    console.log(error);
+})
 
 const app = express();
 
@@ -6,4 +15,4 @@ const PORT = 3200;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}!!!`);
-})
+});
