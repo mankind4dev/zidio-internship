d791218e7776deb7ad615a4995effa90bb60c2fb
mankind4dev <ogunmakin120@gmail.com>
2024-01-09 12:36:38 +0100

    create header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import About from './pages/About'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+      <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />  
-        <Route exact path="/sign-in" element={<SignIn />} />  
-        <Route exact path="/sign-up" element={<SignUp />} />  
-        <Route exact path="/Profile" element={<Profile />} />  
-        <Route exact path="/about" element={<About />} />  
-      </Routes>      
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/Profile" element={<Profile />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
