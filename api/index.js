e45ebe115525b4e708b9744d082012a17f117444
mankind4dev <ogunmakin120@gmail.com>
2024-01-16 04:54:56 +0100

    complete functionalities....

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from"dotenv";
 import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -18,6 +19,9 @@ const app = express();
 const PORT = 3200;
 
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}!!!`);
 })
