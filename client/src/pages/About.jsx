73bb1ae47fe05830f659dee6b92dba9ed52ab29a
mankind4dev <ogunmakin120@gmail.com>
2024-01-21 20:51:50 +0100

    add installation and deploy

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,48 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <img src="" alt="" className="" />
+      <div className="">
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Real Castle
+      </h1>
+      </div>
+      <div className="">
+
+      <p className='mb-4 text-slate-700'>
+        Our suite of online services enables clients 
+        to analyze, interpret and gain unmatched insight 
+        on commercial property values, market conditions
+        and current availabilities. Realcastle.com provides
+        premium data benchmarking, analytics and marketplace 
+        insights for the global hospitality sector. 
+        Realcastle provides a leading platform for conducting 
+        commercial real estate online auctions and 
+        negotiated bids. Realcastle is the most heavily 
+        trafficked commercial real estate marketplace 
+        online. Realla is the Nigeria most comprehensive 
+        commercial property digital marketplace.
+      </p>
+
+      </div>
+      <p className='mb-4 text-slate-700'>
+        Our mission is to help our clients achieve their 
+        real castle goals by providing expert advice, 
+        personalized service, and a deep understanding of 
+        the local market. Whether you are looking to buy, 
+        sell, or rent a property, we are here to help you 
+        every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our team of agents has a wealth of experience and 
+        knowledge in the real castle industry, and we are 
+        committed to providing the highest level of service 
+        to our clients. We believe that buying or selling a
+         property should be an exciting and rewarding 
+         experience, and we are dedicated to making that 
+         a reality for each and every one of our clients.
+        </p>
+    </div>
   )
 }
