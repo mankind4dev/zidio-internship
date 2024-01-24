4e7d09558d87823c67d4cfc8564e3a3c8eb87133
mankind4dev <ogunmakin120@gmail.com>
2024-01-24 04:04:09 +0100

    Home update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
   SwiperCore.use([Navigation]);
-  console.log(offerListings);
+  
 
 
   useEffect(() => {
